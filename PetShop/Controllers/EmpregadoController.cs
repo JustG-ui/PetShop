2e9954dd508d616e038197c69f8de383e5254e6a
JustG-ui <gominhohenrique@gmail.com>
2024-11-09 20:50:42 -0300

    Add project files.

---
+++ PetShop/Controllers/EmpregadoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetShop.Models;
+using System.Diagnostics;
+
+namespace PetShop.Controllers
+{
+    public class EmpregadoController : Controller
+    {
+        private readonly ILogger<EmpregadoController> _logger;
+
+        public EmpregadoController(ILogger<EmpregadoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PetShop/Data/PetShopContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetShop.Models;
+
+namespace PetShop.Data
+{
+    public class PetShopContext : DbContext
+    {
+        public PetShopContext(DbContextOptions<PetShopContext> options) : base(options) { }
+
+    
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Empregados> Fabricantes { get; set; }
+        public DbSet<Servicos> Modelos { get; set; }
+        public DbSet<Animais> Veiculo { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Clientes>()
+            .HasKey(vl => new { vl.clienteId});
+        }
+    }
+}
